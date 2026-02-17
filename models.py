73d6deb0fab3b1e44f777b69e09fc3acaa14e2a0
swastikabarman <155806171+swastikabarman@users.noreply.github.com>
2026-02-17 17:18:12 +0530

    Added Class model

--- models.py
+++ models.py
@@ -1,11 +1,17 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100))
     email = Column(String(100), unique=True)
     password = Column(String(255))
     role = Column(String(20))
+
+
+class Class(Base):
+    __tablename__ = "classes"
+    id = Column(Integer, primary_key=True, index=True)
+    class_name = Column(String(100))
+    teacher_id = Column(Integer, ForeignKey("users.id"))
